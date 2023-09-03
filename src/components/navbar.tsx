d4923655703b80a826979291ae3ecf66b6e28e5b
MG <14829112+Martin-Ocean@users.noreply.github.com>
2023-09-02 22:21:51 -0400

    add navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,14 @@
 "use client"
 import React, { useState, useEffect } from 'react';
+import Hamburger from 'hamburger-react';
 
-const NavBar: React.FC = () => {
+
+type NavBarProps = {
+    inViewOrNo: boolean[];
+}
+const NavBar: React.FC<NavBarProps> = ({ inViewOrNo }) => {
     const [scrolling, setScrolling] = useState<boolean>(false);
+    const [hamburgerOnClick, setHamburgerOnClick] = useState<boolean>(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -19,20 +25,33 @@ const NavBar: React.FC = () => {
         };
     }, []);
 
+    useEffect(() => {
+        console.log(inViewOrNo);
+    }, [inViewOrNo])
+
     return (
-        <nav>
-            <div className="flex flex-wrap items-center justify-between bg-slate-800 md:p-0">
-                <div className="p-3 px-4 text-2xl text-white">Navbar</div>
-                <div className="p-3 md:hidden">
-                   <span>&#9776;</span>
+        <nav className={`fixed flex w-full h-[64px] z-[9] ${scrolling ? 'bg-secondary text-black' : 'bg-black'}`}>
+            <div className="flex flex-wrap items-center md:p-0 justify-between w-full md:mx-12 border-b border-white">
+                <div className={`p-3 px-4 text-2xl  ${scrolling ? 'bg-secondary text-black' : 'text-white'} md:ml-12`}>吃吃派对陪玩零食店</div>
+                <div className="p-3 md:hidden pr-1">
+                    <Hamburger toggled={hamburgerOnClick} toggle={setHamburgerOnClick} />
                 </div>
-                <div id="menu" className="hidden md:block">
+                <div className={`${hamburgerOnClick ? '' : 'hidden'} md:block md:mr-12 ${scrolling ? 'bg-opacity-60 text-black' : 'bg-transparent'} `}>
                     <ul className="items-center w-screen md:w-auto md:flex ">
-                        <li className="p-4 text-gray-300 border-b border-gray-600 md:border-0 "><a href="#">Home</a></li>
-                        <li className="p-4 text-gray-500 border-b border-gray-600 hover:text-gray-300 md:border-0"><a
-                            href="#">Products</a></li>
-                        <li className="p-4 text-gray-500 border-b border-gray-600 hover:text-gray-300 md:border-0"><a
-                            href="#">Pricing</a></li>
+                        <li className={` ${inViewOrNo[0] ? 'border-secondary border-b-2' : `${scrolling ? ' text-black hover:border-black hover:border-b-2' : 'text-white hover:border-secondary hover:border-b-2'}`} `}>
+                            <a href="#section1" className='p-4 flex h-full w-full'>首页</a></li>
+                        <li className={` ${inViewOrNo[1] ? 'border-black border-b-2' : `${scrolling ? ' text-black hover:border-black hover:border-b-2' : 'text-white hover:border-secondary hover:border-b-2'}`} `}>
+                            <a
+                                className='p-4 flex h-full w-full' href="#section2">项目价格</a></li>
+                        <li className={` ${inViewOrNo[2] ? 'border-black border-b-2' : `${scrolling ? ' text-black hover:border-black hover:border-b-2' : 'text-white hover:border-secondary hover:border-b-2'}`}  `}>
+                            <a
+                                className='p-4 flex h-full w-full' href="#section3">零食菜单</a></li>
+                        <li className={` ${inViewOrNo[3] ? 'border-black border-b-2' : `${scrolling ? ' text-black hover:border-black hover:border-b-2' : 'text-white hover:border-secondary hover:border-b-2'}`} `}>
+                            <a
+                                className='p-4 flex h-full w-full' href="#section4">福利活动</a></li>
+                        <li className={` ${inViewOrNo[4] ? 'border-black border-b-2' : `${scrolling ? ' text-black hover:border-black hover:border-b-2' : 'text-white hover:border-secondary hover:border-b-2'}`} `}>
+                            <a
+                                className='p-4 flex h-full w-full' href="#section5">Discord</a></li>
                     </ul>
                 </div>
             </div>
