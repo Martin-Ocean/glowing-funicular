5504853c087f4ed123873f1a5fef152e6b13de81
MG <14829112+Martin-Ocean@users.noreply.github.com>
2023-09-07 05:06:16 -0400

    move card location& adjust styles

--- src/components/ui/GenericCard.tsx
+++ src/components/ui/GenericCard.tsx
@@ -11,39 +11,19 @@ type GenericCardProps = {
     imgOnly?: boolean
 }
 
-const animationList = [
-    'fade',
-    'fade-left',
-    'fade-right',
-    'fade-up-right',
-    'fade-up-left',
-    'fade-down-right',
-    'fade-down-left',
-    "flip-up",
-    "flip-down",
-    "flip-left",
-    "flip-right",
-    "slide-left",
-    "slide-right",
-    "zoom-in",
-    "zoom-in-left",
-    "zoom-in-right",
-    "zoom-out",
-    "zoom-out-left",
-    "zoom-out-right"]
 
 const GenericCard: React.FC<GenericCardProps> = (item) => {
     return (
-        <div data-aos={animationList[Math.floor(Math.random() * animationList.length)]}
+        <div data-aos={'flip-up'} 
             data-aos-offset="50"
             data-aos-delay="50"
             data-aos-duration="500"
-            data-aos-easing="ease-in-out" className={`flex flex-col mx-10 my-4 px-4 py-4 rounded-more ${item.imgOnly ? "w-80vw" : "w-30vw"} generic-card bg-white text-black snap-center`}>
+            data-aos-easing="ease-in-out" className={`flex flex-col justify-between px-4 py-4 h-full rounded-more ${item.imgOnly ? "w-80vw" : "xl:w-20vw md:w-40vw sm:w-70vw"} generic-card bg-white text-black snap-center`}>
             <div className='rounded-less  overflow-hidden'> {/* image or large text for display*/}
                 {item.text}
             </div>
             {(!item.imgOnly) &&
-                <>
+                <div className='mt-atuo'>
                     <div className='flex flex-col mt-8 mb-4 bg-transparent justify-center align-middle text-2xl text-center'> {/* title */}
                         {item.title}
                     </div>
@@ -55,7 +35,7 @@ const GenericCard: React.FC<GenericCardProps> = (item) => {
                         <div className='flex-grow border-t border-gray-400'></div>
                     </div>
                     <div className='flex flex-col  my-2 bg-transparent justify-center align-middle text-md ' > {item.other}</div>
-                </>
+                </div>
             }
         </div>
     );
