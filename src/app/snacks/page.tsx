db92abfde46df96fc1559fe3e5c229226be4b9ef
MG <14829112+Martin-Ocean@users.noreply.github.com>
2023-09-09 20:31:21 -0400

    add snacks page

---
+++ src/app/snacks/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from '@/components/NavBar';
+import ActiveSectionContextProvider from '@/context/active-section-context';
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div className='flex flex-col mx-auto justify-around h-nim-screen'>
+            <main className='h-screen bg-white'>test</main>
+
+        </div>
+    )
+}
